66ea8f8bc0c19916fe8492e7b06b3fe7c3d47d8f
bada308 <71962076+bada308@users.noreply.github.com>
2022-07-17 19:52:42 +0900

    글삭제 구현

--- main.js
+++ main.js
@@ -67,7 +67,15 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${title}`, 'utf-8', function(err, description){
   
             var list = templateList(filelist);
-            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`, 
+              `<a href="/create">create</a>
+              <a href="/update?id=${title}">update</a>
+              <form action="delete_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <input type="submit" value="delete">
+              </form>`
+              
+            );
   
             response.writeHead(200);
             response.end(template);
@@ -156,6 +164,21 @@ var app = http.createServer(function(request,response){
         })
         
       });
+    }
+    else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`, function(err){
+          response.writeHead(302, {Location: `/`});
+          response.end();
+        })
+      })
+        
     }
     else{
       response.writeHead(404);
